bc6cdef2f76876371b66a0770be776cc5f506ecb
Gestalte <66849891+Gestalte@users.noreply.github.com>
2022-07-12 20:44:34 +0200

    Added ability to view, open and drag and drop a markdown file

--- MarkdownReader/MarkdownReader/MainWindow.xaml.cs
+++ MarkdownReader/MarkdownReader/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MarkdownReader
 {
@@ -23,6 +14,96 @@ namespace MarkdownReader
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void loadMarkdown(string markdown)
+        {
+            try
+            {
+                string html = Markdig.Markdown.ToHtml(markdown);
+                browser.NavigateToString(html);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void browser_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            if (e.Uri == null)
+            {
+                // Do nothing
+            }
+            else if (Path.GetExtension(e.Uri.ToString()).ToUpper() == ".MD")
+            {
+                DisplayFile(e.Uri.ToString());
+            }
+            else if (e.Uri != null)
+            {
+                try
+                {
+                    // Attempt to follow link in MD file by opening it in the default browser.
+                    ProcessStartInfo psi = new ProcessStartInfo();
+
+                    psi.FileName = e.Uri.ToString();
+                    psi.UseShellExecute = true;
+
+                    Process.Start(psi);
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void DisplayFile(string path)
+        {
+            if (Path.GetExtension(path).ToUpper() != ".MD")
+            {
+                MessageBox.Show("File must be a markdown file (.md)", "Error");
+                return;
+            }
+
+            if (path.Substring(0,8)=="file:///") // Open drag and drop file.
+            {
+                path = path.Substring(8);
+            }
+
+            string filepath = path;
+
+            pathToFile.Text = filepath;
+
+            string markdown = File.ReadAllText(filepath);
+
+            loadMarkdown(markdown);
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == true)
+            {
+                DisplayFile(ofd.FileName);
+            }
+        }
+
+        private void MenuItem_Click_Exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Grid_Drop(object sender, DragEventArgs e)
+        {
+            string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
+
+            DisplayFile(dataString);
         }
     }
 }
